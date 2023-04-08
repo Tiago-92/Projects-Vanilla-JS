5e2aa31cba85551c5e9a1e0949ec4419489804f2
tiago <tiago@pop-os.localdomain>
2023-04-08 17:28:46 -0300

    finalizado o projeto biscoito da sorte

--- Desafios/biscoito-da-sorte/main.js
+++ Desafios/biscoito-da-sorte/main.js
@@ -1,6 +1,8 @@
 const cookie = document.querySelector('.cookie');
 const openedCookie = document.querySelector('.opened-cookie');
 const btn = document.querySelector('#btn');
+const btn2 = document.querySelector('.btn2');
+const pharse = document.querySelector('.pharse');
 
 const pharses = [
   'Seu código vai rodar sem nenhum bug', 
@@ -14,9 +16,18 @@ const randomPharses = Math.floor(Math.random() * pharses.length);
 function toogleCookie() {
   cookie.classList.toggle('hide');
   openedCookie.classList.toggle('hide');
+  pharse.classList.remove('hide');
+  btn2.classList.remove('hide');
+  btn.disabled = true;
+  
+  btn.querySelector('.pharse').innerText = `${pharses[randomPharses]}`
+  
+}
 
-  btn.querySelector('div').innerText = `${pharses[randomPharses]}`
+function resetApp() {
+  window.location.reload(true);
 }
 
 btn.addEventListener('click', toogleCookie);
+btn2.addEventListener('click', resetApp);
 
